0eca38ab9e7a01a7321e41c991f34bfa66fceea6
Punit Gurnani <punitgurnani@PunitGurnani.local>
2023-01-29 22:00:00 +0530

    Improved singleton pattern

--- src/main/java/studentdirectory/models/UserCollection.java
+++ src/main/java/studentdirectory/models/UserCollection.java
@@ -4,47 +4,59 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
 public final class UserCollection {
 
-  private static final UserCollection USER_COLLECTION = new UserCollection();
+  private static UserCollection userCollection;
 
   private final SortedSet<User> userSet;
 
   private final Map<String, User> rollNoUserMapping;
 
   private UserCollection() {
-    userSet=new TreeSet<>();
+
+    userSet = new TreeSet<>();
     rollNoUserMapping = new HashMap<>();
   }
 
   public static UserCollection getInstance() {
-    return USER_COLLECTION;
+
+    if (Objects.isNull(userCollection)) {
+      userCollection = new UserCollection();
+    }
+    return userCollection;
   }
 
   public void addUser(final User user) {
+
     userSet.add(user);
     rollNoUserMapping.put(user.getRollNo(), user);
   }
 
   public void deleteUser(final String rollNo) {
+
     final User user = rollNoUserMapping.get(rollNo);
     userSet.remove(user);
     rollNoUserMapping.remove(rollNo);
   }
 
   public void clearUserList() {
+
     userSet.clear();
     rollNoUserMapping.clear();
   }
 
   public List<User> getUserList() {
+
     return new ArrayList<>(userSet);
   }
 
   public boolean isUserWithGivenRollNoPresent(final String rollNo) {
+
     return rollNoUserMapping.containsKey(rollNo);
   }
+
 }
